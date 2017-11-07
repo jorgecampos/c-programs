f5ed45f85264c33269114d031653222c9f511185
jorgecampos <jorgemds43@gmail.com>
2017-11-07 12:06:38 +0100

    07/11/2017

---
+++ 00_primitivas/cpp/err.cpp
@@ -0,0 +1,12 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+    fprintf(stderr, "stderr: The world is a vampire.\n");
+    return EXIT_SUCCESS;
+
+}
+
+
---
+++ 00_primitivas/cpp/variadic.cpp
@@ -0,0 +1,18 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+
+    int op1 = 7;
+    int op2;
+  
+    printf("operando 2: ");
+    scanf(" %i",&op2);
+    printf("%i + %i = %i\n", op1, op2, op1 + op2);
+  
+    return EXIT_SUCCESS;
+
+}
+
+
